b3a20a74d9feff65b6129fcb84cfc9bf71461cdd
Juergen Key <sgjfsf@hotmail.com>
2019-11-02 13:37:13 +0100

    some new generators

---
+++ src/main/java/de/elbosso/microgenerator/date/DateResource.java
@@ -0,0 +1,11 @@
+package de.elbosso.microgenerator.date;
+
+import de.elbosso.microgenerator.ValueResource;
+
+public class NumberResource extends ValueResource<Number>
+{
+	NumberResource(Number value)
+	{
+		super(value);
+	}
+}
---
+++ src/main/java/de/elbosso/microgenerator/string/FamilyBusinessResource.java
@@ -0,0 +1,5 @@
+package de.elbosso.microgenerator.string;
+
+public class FamilyBusinessResource
+{
+}
